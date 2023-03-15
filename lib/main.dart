0d76fad61e5374bab9cf22b46689e5dbe3c05f7b
bolaji Adeladan <bolaji.adeladan@gmail.com>
2023-03-15 11:08:03 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grazac_chat_app/screens/chat_screen.dart';
 import 'package:grazac_chat_app/screens/login_screen.dart';
 import 'package:grazac_chat_app/screens/registration_screen.dart';
 import 'package:grazac_chat_app/screens/welcome_screen.dart';
 
-void main() => runApp(GrazacChat());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  
+  runApp(GrazacChat());
+}
 
 class GrazacChat extends StatelessWidget {
   @override
